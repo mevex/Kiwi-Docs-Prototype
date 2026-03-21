2c7bb7b4c6de8936b02ef203c8fc56a0b90cebf5
Valentino <mevex98@gmail.com>
2026-03-21 20:40:04 +0100

    Config 5

--- quartz/quartz.config.ts
+++ quartz/quartz.config.ts
@@ -2,6 +2,8 @@ import { CustomOgImages } from "./quartz/plugins/emitters/ogImage"
 
 const config: QuartzConfig = {
 	plugins: {
+		transformers: [],
+		filters: [],
 		emitters: [
 			CustomOgImages({
 				// colorScheme: "lightMode", // what colors to use for generating image, same as theme colors from config, valid values are "darkMode" and "lightMode"
