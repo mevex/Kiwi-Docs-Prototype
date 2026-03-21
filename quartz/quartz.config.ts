f66e815be115338dcfcf7dcac64311ae80eaddf9
Valentino <mevex98@gmail.com>
2026-03-21 19:37:43 +0100

    Config 1

---
+++ quartz/quartz.config.ts
@@ -0,0 +1,15 @@
+import { CustomOgImages } from "./quartz/plugins/emitters/ogImage"
+
+const config: QuartzConfig = {
+	plugins: {
+		emitters: [
+			CustomOgImages({
+				// colorScheme: "lightMode", // what colors to use for generating image, same as theme colors from config, valid values are "darkMode" and "lightMode"
+				width: 160, // width to generate with (in pixels)
+				height: 160, // height to generate with (in pixels)
+				excludeRoot: false, // wether to exclude "/" index path to be excluded from auto generated images (false = use auto, true = use default og image)
+				imageStructure: "/assets/install-preview.png", // custom image component to use
+			}),
+		],
+	},
+}
