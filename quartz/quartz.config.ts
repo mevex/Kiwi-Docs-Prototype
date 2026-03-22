212ff20bff0c424ea3f5cb8dc1a353ad8b513783
Valentino <mevex98@gmail.com>
2026-03-22 01:47:53 +0100

    Config 10

--- quartz/quartz.config.ts
+++ quartz/quartz.config.ts
@@ -58,10 +58,6 @@ const config: QuartzConfig = {
 			Plugin.ContentPage(),
 			Plugin.FolderPage(),
 			Plugin.TagPage(),
-			Plugin.ContentIndex({
-				enableSiteMap: true,
-				enableRSS: true,
-			}),
 			Plugin.Assets(),
 			Plugin.Static(),
 			Plugin.NotFoundPage(),
