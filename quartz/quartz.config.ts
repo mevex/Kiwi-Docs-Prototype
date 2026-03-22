ab4d91557e8706ff9b84883baa58aef88b160cbd
Valentino <mevex98@gmail.com>
2026-03-22 01:41:34 +0100

    Config 7

--- quartz/quartz.config.ts
+++ quartz/quartz.config.ts
@@ -1,4 +1,42 @@
+import { QuartzConfig } from "./quartz/cfg"
+import * as Plugin from "./quartz/plugins"
 import { CustomOgImages } from "./quartz/plugins/emitters/ogImage"
+
+const config: QuartzConfig = {
+	configuration: {
+		pageTitle: "Kiwi Documentation",
+		enableSPA: true,
+		enablePopovers: true,
+		analytics: null,
+		theme: {
+			fontOrigin: "googleFonts",
+			cdnCaching: true,
+			typography: {
+				title: "Roboto",
+				header: "Roboto",
+				body: "Roboto",
+				code: "JetBrains Mono",
+			},
+			colors: {
+				lightMode: {
+					light: "#2b2b2b",
+					// lightgray: "rgba(78, 78, 78, 1.0)",
+					lightgray: "rgba(255, 78, 78, 1.0)",
+				},
+				darkMode: {
+					light: "#161618",
+					// lightgray: "rgb(57, 54, 57, 1.0)",
+					lightgray: "rgb(57, 255, 57, 1.0)",
+				}
+			}
+		}
+	},
+	plugins: {
+		transformers: [],
+		filters: [],
+		emitters: []
+	}
+}
 
 const config: QuartzConfig = {
 	configuration: {
