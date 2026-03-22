45d553f7de5e4c1b911febbcab40aadfe179f406
Valentino <mevex98@gmail.com>
2026-03-22 01:43:24 +0100

    Config 8

--- quartz/quartz.config.ts
+++ quartz/quartz.config.ts
@@ -38,22 +38,4 @@ const config: QuartzConfig = {
 	}
 }
 
-const config: QuartzConfig = {
-	configuration: {
-		ignorePatterns: [],
-	},
-	plugins: {
-		transformers: [],
-		filters: [],
-		emitters: [
-			CustomOgImages({
-				// colorScheme: "lightMode", // what colors to use for generating image, same as theme colors from config, valid values are "darkMode" and "lightMode"
-				width: 160, // width to generate with (in pixels)
-				height: 160, // height to generate with (in pixels)
-				excludeRoot: false, // wether to exclude "/" index path to be excluded from auto generated images (false = use auto, true = use default og image)
-			}),
-		],
-	},
-}
-
 export default config
